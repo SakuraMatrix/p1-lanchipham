718cd3e59f181eb87edcd2b89745947bdb804038
LanChi Pham <Lpham2525@gmail.com>
2021-08-06 09:28:45 -0400

    added first file Budget.java

---
+++ src/main/java/com.github.SakuraMatrix.p1-lanchipham/Budget.java
@@ -0,0 +1,11 @@
+package com.github.SakuraMatrix.p1-lanchipham;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Budget{
+    public static void main(String90 args) {
+        Logger log = LoggerFactory.getLogger(Budget.class);
+        log.info("Starting com.github.SakuraMatrix.p1-lanchipham...");
+    }
+}
